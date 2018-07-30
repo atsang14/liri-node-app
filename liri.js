eff6e7e875a6da4f833ba4630cbb9d99d56141ed
aytsang14@gmail.com <aytsang14@gmail.com>
2018-07-30 11:35:32 -0700

    now shows spotify Artists Song name etc when using spotify-this-song command

--- liri.js
+++ liri.js
@@ -1,18 +1,28 @@
 var dotenv 		= require("dotenv").config();
 var Twitter 	= require('twitter');
-var keys    	= require('./keys.js');
-var fs 			= require('fs');
+var Spotify 	= require('node-spotify-api');
+var request 	= require("request");
 var command 	= process.argv[2];
-var request     = require('request');
+var songName	= process.argv[3];
+
+
 
 var client = new Twitter({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+	consumer_key: process.env.TWITTER_CONSUMER_KEY,
+	consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+	access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+	access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-////////////////////my tweets//////////////////////////////
+var spotify = new Spotify({
+  	id: process.env.SPOTIFY_ID,
+  	secret: process.env.SPOTIFY_SECRET
+});
+
+/////////////////////////////////////////////////////////
+////////////////////my-tweets////////////////////////////
+/////////////////////////////////////////////////////////
+
 if (command == 'my-tweets') {
 	client.get('statuses/home_timeline', function(error, tweets, response) {
 		
@@ -25,6 +35,116 @@ if (command == 'my-tweets') {
 	});
 } 
 
+/////////////////////////////////////////////////////////
+////////////////////Spotify//////////////////////////////
+/////////////////////////////////////////////////////////
+
+else if(command == 'spotify-this-song') {
+	if(songName != null) {
+
+		spotify.search({ type: 'track', query: songName })
+	  	.then(function(response) {
+	  		
+	  		var artists = response.tracks.items[0].album.artists;
+			
+			// loops over all the artists  	
+	  		for(var i = 0; i < artists.length; i++) {
+	  		
+	  			console.log(response.tracks.items[0].album.artists[i].name);	
+	  		
+	  		}
+
+	  		// song name
+			console.log(response.tracks.items[0].name)
+
+	  		// album name
+			console.log(response.tracks.items[0].album.name);
+
+			// preview link
+			console.log(response.tracks.items[0].album.external_urls.spotify);
+
+	  		}).catch(function(err) {
+	    	console.log(err);
+		});
+  	} else if (songName == null || songName == '') {
+  		spotify.search({ type: 'track', query: 'The Sign', limit: 50 })
+	  	.then(function(response) {
+	  		
+	  		var x = '';
+
+			for(var i = 0; i <response.tracks.items.length; i++ ) {
+				
+				for(var j = 0; j < response.tracks.items[i].artists.length; j++ ) {
+					
+					if ((response.tracks.items[i].artists[j].name == 'Ace of Base') && (response.tracks.items[i].name == 'The Sign')) {
+						x = i;
+					}
+				}
+			}	
+
+	  		var artists = response.tracks.items[x].album.artists;
+			
+			// loops over all the artists  	
+	  		for(var i = 0; i < artists.length; i++) {
+	  		
+	  			console.log('artists ' + response.tracks.items[x].album.artists[i].name);	
+	  		
+	  		}
+
+	  		// song name
+			console.log(response.tracks.items[x].name)
+			console.log('===============');
+	  		// album name
+			console.log(response.tracks.items[x].album.name);
+			console.log('===============');
+			// preview link
+			console.log(response.tracks.items[x].album.external_urls.spotify);
+			console.log('===============');
+
+	  		}).catch(function(err) {
+	    	console.log(err);
+		});
+  	}
+}
+/////////////////////////////////////////////////////////
+////////////////////Movie////////////////////////////////
+/////////////////////////////////////////////////////////
+else if(command == 'movie-this') {
+	
+	var movieName	= process.argv[3];
+
+	if (movieName != null) {
+		request("http://www.omdbapi.com/?t="+movieName+"=&plot=short&apikey=trilogy", function(error, response, body) {
+
+			  // If the request is successful (i.e. if the response status code is 200)
+			if (!error && response.statusCode === 200) {
+
+			    console.log('===============');
+			    console.log(JSON.parse(body).Title);
+			    console.log('===============');
+			    console.log(JSON.parse(body).Released);
+			    console.log('===============');
+			    console.log(JSON.parse(body).imdbRating);
+			    console.log('===============');
+			    console.log(JSON.parse(body).Ratings[2].Source);
+			    console.log('===============');
+			    console.log(JSON.parse(body).Country);
+			    console.log('===============');
+			    console.log(JSON.parse(body).Language);
+			    console.log('===============');
+			    console.log(JSON.parse(body).Plot);
+			    console.log('===============');
+			    console.log(JSON.parse(body).Actors);
+			    console.log('===============');
+			    
+			} 
+		});
+	} else if(movieName == null) {
+		console.log(`If you haven't watched "Mr. Nobody," then you should: <http://www.imdb.com/title/tt0485947/>`)
+		console.log("It's on Netflix!");
+	}
+	
+}
 
 
 
@@ -33,12 +153,11 @@ if (command == 'my-tweets') {
 
 
 
+//////////////////////////////testing//////////////////////////////
 
-
-
-
-
-
+// var keys    	= require('./keys.js');
+// var fs 			= require('fs');
+// var request     = require('request');
 
 // client.get('statuses/home_timeline', function(error, tweets, response) {
 //   if(error) throw error;
