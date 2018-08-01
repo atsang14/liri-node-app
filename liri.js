36c4c7d881c29eb24da335847adbfa32b9b93741
aytsang14@gmail.com <aytsang14@gmail.com>
2018-08-01 00:49:07 -0700

    connected keys.js and use the exports from there instead of jumping straight into the .env file

--- liri.js
+++ liri.js
@@ -1,23 +1,15 @@
-var dotenv 		= require("dotenv").config();
+require("dotenv").config();
 var Twitter 	= require('twitter');
 var Spotify 	= require('node-spotify-api');
 var request 	= require("request");
 var fs 			= require('fs');
+var keys 		= require('./keys.js')
 var command 	= process.argv[2];
 var songName	= process.argv[3];
 
 //////////////////////Keys////////////////////////////////
-var client = new Twitter({
-	consumer_key: process.env.TWITTER_CONSUMER_KEY,
-	consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-	access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-	access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-});
-
-var spotify = new Spotify({
-  	id: process.env.SPOTIFY_ID,
-  	secret: process.env.SPOTIFY_SECRET
-});
+var spotify		= new Spotify(keys.spotify);
+var client 		= new Twitter(keys.twitter);
 
 /////////////////////////////////////////////////////////
 ////////////////////my-tweets////////////////////////////
@@ -40,6 +32,7 @@ if (command == 'my-tweets') {
 
 			console.log(tweets[i].created_at + ': ' + tweets[i].text);
 			var text = JSON.stringify(tweets[i].text);
+			
 			fs.appendFile("log.txt", text+' ', function(err) {
 		    // If there was an error, we log it and return immediately
 			    if (err) {
@@ -74,12 +67,15 @@ else if(command == 'spotify-this-song') {
 
 	  		// song name
 			console.log(response.tracks.items[0].name)
+			console.log('===============');
 
 	  		// album name
 			console.log(response.tracks.items[0].album.name);
+			console.log('===============');
 
 			// preview link
 			console.log(response.tracks.items[0].album.external_urls.spotify);
+			console.log('===============');
 
 	  		}).catch(function(err) {
 	    	console.log(err);
@@ -112,9 +108,11 @@ else if(command == 'spotify-this-song') {
 	  		// song name
 			console.log(response.tracks.items[x].name)
 			console.log('===============');
+
 	  		// album name
 			console.log(response.tracks.items[x].album.name);
 			console.log('===============');
+			
 			// preview link
 			console.log(response.tracks.items[x].album.external_urls.spotify);
 			console.log('===============');
@@ -136,23 +134,25 @@ else if(command == 'movie-this') {
 
 			  // If the request is successful (i.e. if the response status code is 200)
 			if (!error && response.statusCode === 200) {
-
+				
+				var json = JSON.parse(body);
+			    
 			    console.log('===============');
-			    console.log(JSON.parse(body).Title);
+			    console.log(json.Title);
 			    console.log('===============');
-			    console.log(JSON.parse(body).Released);
+			    console.log(json.Released);
 			    console.log('===============');
-			    console.log(JSON.parse(body).imdbRating);
+			    console.log(json.imdbRating);
 			    console.log('===============');
-			    console.log(JSON.parse(body).Ratings[2].Source);
+			    console.log(json.Ratings[2].Source);
 			    console.log('===============');
-			    console.log(JSON.parse(body).Country);
+			    console.log(json.Country);
 			    console.log('===============');
-			    console.log(JSON.parse(body).Language);
+			    console.log(json.Language);
 			    console.log('===============');
-			    console.log(JSON.parse(body).Plot);
+			    console.log(json.Plot);
 			    console.log('===============');
-			    console.log(JSON.parse(body).Actors);
+			    console.log(json.Actors);
 			    console.log('===============');
 			    
 			} 
@@ -198,90 +198,13 @@ else if(command == 'do-what-it-says') {
 			console.log(response.tracks.items[0].album.external_urls.spotify);
 
 	  		}).catch(function(err) {
-	    	console.log(err);
+
+	    		console.log(err);
+
 		});
 	});
 
 }
-
-
-
-
-
-
-//////////////////////////////testing//////////////////////////////
-
-// var keys    	= require('./keys.js');
-
-// var request     = require('request');
-
-// client.get('statuses/home_timeline', function(error, tweets, response) {
-//   if(error) throw error;
-//   console.log(tweets);  // The favorites.
-//   console.log('===============');
-//   console.log(response);  // Raw response object.
-// });
-
-// var processENV  = process.env;
-
-// console.log('======Twitter========');
-
-// console.log(processENV);
-
-
-
-// console.log(processENV.TWITTER_CONSUMER_KEY);
-
-// console.log('=======Keys========');
-
-// console.log(dotenv);
-
-// console.log('===============');
-
-// console.log(keys.twitter);
-
-// console.log('=======Client========');
-
-// var spotify = new Spotify(keys.spotify);
-
-// var client = new Twitter(keys.twitter);
-
-// console.log(client);
-
-// console.log('===============');
-
-// client.get('favorites/list', function(error, tweets, response) {
-//   if(error) throw error;
-//   console.log(tweets);  // The favorites.
-
-//   console.log('===============');
-
-//   console.log(response);  // Raw response object.
-// });
-
-
-// client.post('statuses/update', {status: 'Hi'}, function(error, tweet, response) {
-//   if (!error) {
-//     console.log(tweet);
-//   }
-// });
-
-
-// var request = require('request');
-
-// request("https://api.twitter.com/1.1/statuses/home_timeline.json", function(error, response, body) {
-
-//   // If the request is successful (i.e. if the response status code is 200)
-//   if (!error && response.statusCode === 200) {
-
-//     // Parse the body of the site and recover just the imdbRating
-//     // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-//     console.log(response);
-//     console.log(body);
-//   }
-// });
-
-
 
 
 
